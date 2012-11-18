9535f6765808597acf554152686e3d909ddac9d5
mehmoodz <mehmoods2u@gmail.com>
2012-11-18 07:00:13 -0500

    Design Changes commit V2

--- app/public/js/serverjack.js
+++ app/public/js/serverjack.js
@@ -1,4 +1,5 @@
-var eventGridHTML='<div class="events" id="event_$eventId" onclick="openRegistration(\'$eventUrl\;)"><table><tr><div id="name_box">$eventName</div></tr><tr><br/></tr><tr><div id="image_box" width="200"><img src="$eventImg" alt="Profile pic" width="240" height="240" /></div></tr><tr><div id="description_box" width="200"><p >Type: $evenType &nbsp; Date:$eventDate &nbsp; Location:$eventLocation </p><p >$evenDesc </p></div></tr></table></div>';
+//var eventGridHTML='<div class="events" id="event_$eventId" onclick="openRegistration(\'$eventUrl\;)"><table><tr><div id="name_box">$eventName</div></tr><tr><br/></tr><tr><div id="image_box" width="200"><img src="$eventImg" alt="Profile pic" width="240" height="240" /></div></tr><tr><div id="description_box" width="200"><p >Type: $evenType &nbsp; Date:$eventDate &nbsp; Location:$eventLocation </p><p >$evenDesc </p></div></tr></table></div>';
+var eventGridHTML='<a href="javascript:void(0)" onclick="openRegistration(\'$eventUrl\')"><div class="polaroid" id="$eventId" style="display:none;"  ><p  class="head" style="background-color:#000;opacity:0.8;display:none;">$evenDesc</p><p ><span style="font-size:15px;color:#f6931f"> $eventName : $evenType </span> <br/> $eventDate, $eventLocation </p><img src="$eventImg" /></div></a>';
 
 var timeFrame={
 		WEEK:"WEEK",
@@ -28,7 +29,8 @@ function mapEvents(eventTimeFrame,activityType){
 }
 //Load the grid data
 function filterGrid(hardLevel){
-	$.getJSON('/events?type='+hardLevel,function(data){
+	//'/events?type='+hardLevel
+	$.getJSON("response.json",function(data){
 		$.each(data,function(index,eventInfo){
 			$(mapEvents(eventInfo.time_frame,eventInfo.activity_type)).append(eventGridHTML.replace("$eventName",eventInfo.name)
 					.replace("$eventImg",eventInfo.image_url)
@@ -39,8 +41,10 @@ function filterGrid(hardLevel){
 					.replace("$eventUrl",eventInfo.url)
 					.replace("$eventLocation",eventInfo.location)
 					);
+			$('#'+eventInfo.id).fadeIn(500);
+			
 		});
-		
+		eventCallBack();
 		
 	});
 }
@@ -54,4 +58,36 @@ function loadUser(){
 		$('#profile-user').html(row.name);
 		
 	});
+}
+
+function openRegistration(url,txt){
+	
+	// get the screen height and width  
+	var maskHeight = $(document).height();  
+	var maskWidth = $(window).width();
+	
+	// calculate the values for center alignment
+	var dialogTop =  (maskHeight/3) - ($('#dialog-box').height());  
+	var dialogLeft = (maskWidth/2) - ($('#dialog-box').width()/2); 
+	
+	// assign values to the overlay and dialog box
+	$('#dialog-overlay').css({height:maskHeight, width:maskWidth}).show();
+	$('#dialog-box').css({top:dialogTop, left:dialogLeft}).show();
+	
+	// display the message
+	$('#dialog-message').html('<div style="font-size:15px;"> Congratulations !! You have signed for the event! <a href="#" style="color:#000;">Click Here</a> to learn more about the event.</div>');
+	
+	// if user clicked on button, the overlay layer or the dialogbox, close the dialog	
+	$('a.btn-ok, #dialog-overlay, #dialog-box').click(function () {		
+		$('#dialog-overlay, #dialog-box').hide();		
+		return false;
+	});
+	
+	// if user resize the window, call the same function again
+	// to make sure the overlay fills the screen and dialogbox aligned to center	
+	$(window).resize(function () {
+		
+		//only do it if the dialog box is not hidden
+		if (!$('#dialog-box').is(':hidden')) popup();		
+	});	
 }
